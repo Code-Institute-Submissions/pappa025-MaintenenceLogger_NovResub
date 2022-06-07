3b436d106c4193dc6aae4c51450d111d27da5b7a
Adam Papp <pappadam025@gmail.com>
2022-06-07 20:49:44 +0000

    Setup the menu and welcome text, added text to instructions option

--- run.py
+++ run.py
@@ -17,6 +17,40 @@ SHEET = GSPREAD_CLIENT.open('flight_log')
 
 flights = SHEET.worksheet('flights')
 
-data = flights.get_all_values()
+print("-=<   Welcome in our Flight Club's Electronic Flight Logger   >=-\n")
+print("   Hope you find this program useful addition to your Logbook!\n")
 
-print(data)
+def menu():
+    print("Please choose one of the following options:")
+    print("[1] Option 1 - Instructions")
+    print("[2] Option 2 - Last engine maintenence time")
+    print("[3] Option 3 - Log a flight")
+    print("[0] Exit")
+
+def instructions():
+    print("")
+    print("You can choose the desired option from the main menu.") 
+    print("If you choose 0, the program will exit!")
+
+menu()
+option = int(input("Enter your option: "))
+
+while option != 0:
+    if option == 1:
+        #option 1 - instructions
+        instructions()
+    elif option == 2:
+        #option 2 - last engine maintenecae time
+        print("Option 2 has been called")
+    elif option == 3:
+        #option 3 - log a flight
+        print("Option 3 has been called")
+    else:
+        print("Invalid option.")
+
+    print()
+    menu()
+    option = int(input("Entert your option: "))
+
+print("Thanks for using this program!")
+
